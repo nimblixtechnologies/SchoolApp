87ddac639f779eb5c14fbf4537e99674d9f1c0de
NimblixTechnologies <tejaswininimblixtechnologies@gmail.com>
2026-01-20 08:48:57 +0530

    git first commit.
    git first commit.

--- SchoolPEPProject/src/main/java/com/nimblix/SchoolPEPProject/Repository/ParentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nimblix.SchoolPEPProject.Repository;
-
-import com.nimblix.SchoolPEPProject.Model.Parent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParentRepository extends JpaRepository<Parent,Long> {
-
-    boolean existsByEmailId(String emailId);
-}
